f5f11b7a1e4f31121a228bb7751a1e34b75e1a92
Daniel Dereniowski <leinad885@o2.pl>
2021-04-07 17:41:16 +0200

    Some design and form completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { faBug, faBinoculars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bug-tracker';
+  constructor() {}
+
+  //font awesome icons
+  faBug = faBug;
+  faBinoculars = faBinoculars;
+
+  ngOnInit(): void{
+  
+  }
 }
