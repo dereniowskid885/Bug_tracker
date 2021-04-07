7fadc90f4d22149d3d824aefb9186fdc1594242d
Daniel Dereniowski <leinad885@o2.pl>
2021-04-07 22:27:01 +0200

    Menu and animated sections added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { faBug, faBinoculars } from '@fortawesome/free-solid-svg-icons';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,22 @@ export class AppComponent implements OnInit{
   faBinoculars = faBinoculars;
 
   ngOnInit(): void{
-  
+    // divs hide and show on button click in home container
+    $(document).ready(function() {
+      $("#add_button").click(function() {
+        $(".home > div:not(.bug_add").hide("fast");
+        $(".bug_add").show("slow");
+      });
+
+      $("#list_button").click(function() {
+        $(".home > div:not(.bug_list").hide("fast");
+        $(".bug_list").show("slow");
+      });
+
+      $("#about_button").click(function() {
+        $(".home > div:not(.bug_about").hide("fast");
+        $(".bug_about").show("slow");
+      });
+    });
   }
 }
